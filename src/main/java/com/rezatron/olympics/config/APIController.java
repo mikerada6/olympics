35ee013060faaa83de19cddb28a1910fec8d78aa
mradas341 <michael_radaszkiewicz@comcast.com>
2023-10-26 15:40:58 -0400

    basic setup

---
+++ src/main/java/com/rezatron/olympics/config/APIController.java
@@ -0,0 +1,20 @@
+package com.rezatron.olympics.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/dashboard")
+@Slf4j
+public class APIController {
+    @GetMapping(value = "/dashboard")
+    public ResponseEntity.BodyBuilder dashboard() {
+        return ResponseEntity.status(HttpStatus.OK);
+    }
+}
