ece25b2655d2330512569b74d64add2ebe054b38
michael <mrada6@gmail.com>
2024-04-24 07:50:55 -0400

    initial commit

---
+++ src/test/java/org/rezatron/olympics/OlympicsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rezatron.olympics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
